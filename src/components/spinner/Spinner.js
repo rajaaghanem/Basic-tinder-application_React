a8553cef8f126ac661a92694f9e756427b612ac9
rajaaghanem <rajaaghanem@gmail.com>
2021-12-23 15:52:40 +0200

    style

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,11 +1,8 @@
 import React from "react";
-import "./Spinner.css"
+import "./Spinner.css";
 class Spinner extends React.Component {
-
   render() {
-    return (
-      <div className="circle"></div>
-    );
+    return <div className="circle"></div>;
   }
 }
 
