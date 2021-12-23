5ce05213ddf778f1fa4c960dfb88c140e5e2b814
rajaaghanem <rajaaghanem@gmail.com>
2021-12-23 15:15:06 +0200

    handle click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
 if (module.hot) {
@@ -6,18 +7,45 @@ if (module.hot) {
 }
 
 class App extends React.Component {
-  state = { like: 0, dislike: 0 };
+  state = {
+    like: 0,
+    dislike: 0,
+    photos: [],
+    count: 0,
+    currentImg: "",
+    currentName: "",
+  };
 
-// changing the state depends on the user input
-  handleLike=(e)=>{
-    const name =`${e.target.name}`;
-    console.log(name);
-    this.setState((state)=>{
-         return {[name]: state[name]+1};
-        })
-  }
+  componentDidMount = async () => {
+    const res = await axios.get(
+      "https://61c468f0f1af4a0017d99514.mockapi.io/animals"
+    );
+    this.setState({ photos: res.data });
+  };
+
+  // changing the state depends on the user input
+  handleLike = (e) => {
+    const name = `${e.target.name}`;
+    this.setState((state) => {
+      return { [name]: state[name] + 1, count: state.count + 1 };
+    });
+    this.createImg();
+  };
+
+  createImg = () => {
+    this.setState({
+      currentImg: (
+        <img
+          src={`${this.state.photos[this.state.count].img}`}
+          className="img-container"
+        />
+      ),
+      currentName: this.state.photos[this.state.count].name,
+    });
+  };
 
   render() {
+    console.log(this.state.photos);
     return (
       <div className="app-container">
         <div className="state-container">
@@ -25,10 +53,16 @@ class App extends React.Component {
           <div>{`dislike: ${this.state.dislike}`}</div>
         </div>
 
-        <div className="img-container">img</div>
+        <div className="img-container">
+          {this.state.currentName}
+          {this.state.currentImg}
+        </div>
         <div className="buttons-container">
-          <button onClick={this.handleLike} name="like">like</button>
-          <button onClick={this.handleLike} name="dislike">dislike</button>
+          <button onClick={this.handleLike} name="like" className="like-btn">
+            
+          </button>
+          <button onClick={this.handleLike} name="dislike" className="dislike-btn">
+          </button>
         </div>
       </div>
     );
