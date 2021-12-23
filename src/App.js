a5aab1fb735c5f035cb41f3afb02a803fc5b48a7
rajaaghanem <rajaaghanem@gmail.com>
2021-12-23 13:55:46 +0200

    start

--- src/App.js
+++ src/App.js
@@ -6,8 +6,21 @@ if (module.hot) {
 }
 
 class App extends React.Component {
+  state = { like: 0, dislike: 0 };
+
   render() {
-    return <></>;
+    return (
+      <div className="app-container">
+        <div className="state-container">
+          <div>{this.state.like}</div>
+          <div>{this.state.dislike}</div>
+        </div>
+
+        <div className="img-container">img</div>
+        <button>like</button>
+        <button>dislike</button>
+      </div>
+    );
   }
 }
 
