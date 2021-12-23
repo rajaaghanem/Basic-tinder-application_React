51a27f6a4f8e1c9cc8c51da01434ff124734f684
rajaaghanem <rajaaghanem@gmail.com>
2021-12-23 13:34:33 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class App extends React.Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default App;
